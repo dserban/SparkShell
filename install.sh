f45d1813e9310dff7fb986a8a2fad8bb7806e74b
Dan Serban <dserban01@yahoo.com>
2018-08-11 16:04:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get -qq update
 apt-get -qq install --no-install-recommends -y \
-  wget curl git vim tmux jq mc net-tools less  \
-  htop unzip locales                           \
+  wget curl git vim tmux jq mc lynx net-tools  \
+  less htop unzip locales                      \
   ca-certificates build-essential              \
   librdkafka-dev libev-dev libsnappy-dev zlib1g-dev netcat-traditional >/dev/null
 
@@ -11,34 +11,35 @@ echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen >/dev/null
 echo -e '\nexport LANG=en_US.UTF-8' >> /root/.bashrc
 
-echo 'Downloading JDK ...'
-wget -qO /opt/zzzjdk.tgz \
-         https://cdn.azul.com/zulu/bin/zulu8.30.0.1-jdk8.0.172-linux_x64.tar.gz
-echo 'Extracting JDK ...'
-tar -xf /opt/zzzjdk.tgz -C /opt
+echo 'Setting up the JDK ...'
+JDK_TGZ_URL=$(lynx -dump https://www.azul.com/downloads/zulu/zulu-linux/ | grep -o http.*jdk8.*x64.*gz$ | head -1)
+echo "From ${JDK_TGZ_URL}"
+wget -qO /opt/tmp/zzzjdk.tgz ${JDK_TGZ_URL}
+tar -xf /opt/tmp/zzzjdk.tgz -C /opt
 mv /opt/zulu* /opt/jdk
-rm /opt/zzzjdk.tgz
+rm /opt/tmp/zzzjdk.tgz
 
 CLOSER="https://www.apache.org/dyn/closer.cgi?as_json=1"
 MIRROR=$(curl --stderr /dev/null ${CLOSER} | jq -r '.preferred')
 
-echo 'Downloading Spark ...'
-wget -qO /opt/zzzspark.tgz \
-         ${MIRROR}spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz
-echo 'Extracting Spark ...'
-tar -xf /opt/zzzspark.tgz -C /opt
+echo 'Setting up Spark ...'
+SPARK_DIR_URL=$(lynx -dump ${MIRROR}spark/ | grep -o 'http.*/spark/spark-[0-9].*$' | sort -V | tail -1)
+SPARK_TGZ_URL=$(lynx -dump ${SPARK_DIR_URL} | grep -o http.*bin-hadoop.*tgz$ | tail -1)
+echo "From ${SPARK_TGZ_URL}"
+wget -qO /opt/tmp/zzzspark.tgz ${SPARK_TGZ_URL}
+tar -xf /opt/tmp/zzzspark.tgz -C /opt
 mv /opt/spark-* /opt/spark
-rm /opt/zzzspark.tgz
+rm /opt/tmp/zzzspark.tgz
 cd /opt/spark/conf
 sed 's/INFO/FATAL/;s/WARN/FATAL/;s/ERROR/FATAL/' log4j.properties.template > log4j.properties
 
-echo 'Downloading Hadoop ...'
-wget -qO /opt/zzzhadoop.tgz \
-         ${MIRROR}hadoop/common/hadoop-2.7.5/hadoop-2.7.5.tar.gz
-echo 'Extracting Hadoop ...'
-tar -xf /opt/zzzhadoop.tgz -C /opt
+echo 'Setting up Hadoop ...'
+HADOOP_TGZ_URL=$(lynx -dump ${MIRROR}hadoop/common/stable/ | grep -o http.*gz$ | grep -v src | head -1)
+echo "From ${HADOOP_TGZ_URL}"
+wget -qO /opt/tmp/zzzhadoop.tgz ${HADOOP_TGZ_URL}
+tar -xf /opt/tmp/zzzhadoop.tgz -C /opt
 mv /opt/hadoop-* /opt/hadoop
-rm /opt/zzzhadoop.tgz
+rm /opt/tmp/zzzhadoop.tgz
 
 echo 'Downloading MovieLens File ...'
 wget -qO /opt/zzzmovielens.zip \
@@ -54,15 +55,14 @@ cut -d, -f1-3 /opt/movielens/ratings.csv.orig | tail -n +2 > /opt/movielens/rati
 mv movies.csv movies.csv.orig
 tail -n +2 movies.csv.orig > movies.csv
 
-echo 'Downloading Scala ...'
-wget -qO /opt/zzzscala.tgz \
-         https://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz
+echo 'Setting up Scala 2.11.8 ...'
+wget -qO /opt/zzzscala.tgz https://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz
 echo 'Extracting Scala ...'
 tar -xf /opt/zzzscala.tgz -C /opt
 mv /opt/scala-* /opt/scala
 rm /opt/zzzscala.tgz
 
-echo 'Installing sbt ...'
+echo 'Setting up sbt 1.2.1 ...'
 export SBTV=1.2.1
 wget -qO /opt/zzzsbt.tgz https://github.com/sbt/sbt/releases/download/v${SBTV}/sbt-${SBTV}.tgz
 tar -xf /opt/zzzsbt.tgz -C /opt
